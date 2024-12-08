b89c922fe19d5716becb1d5cba64cc6a3389d429
Devansh15052004 <142193537+Devansh15052004@users.noreply.github.com>
2024-12-08 04:37:13 +0000

    function

---
+++ Function/01_function.js
@@ -0,0 +1,13 @@
+function myfirstfunction(){
+    console.log("hello");
+    
+}
+// myfirstfunction---->Reference
+// myfirstfunction()----->call 
+
+function addTwoNumber(number1,Number2){
+    console.log(number1+Number2);
+    
+}
+// addTwoNumber(10,10);---->20
+ 
